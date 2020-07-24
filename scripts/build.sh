fcd04e198c80ed0f6e49845957732d5109efde8c
OleksandrBlack <aleksandr.chornuy@gmail.com>
2020-07-24 08:44:14 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ WORKDIR="$(pwd)"
 BUILD_DIR=$(mktemp -d)
 COREDNS_VERSION="v1.7.0"
 
-git clone --branch ${COREDNS_VERSION} https://github.com/coredns/coredns ${BUILD_DIR}
+git clone --branch ${COREDNS_VERSION} https://github.com/OleksandrBlack/coredns ${BUILD_DIR}
 cd ${BUILD_DIR}
 echo "dnsseed:github.com/oleksandrblack/dnsseeder/dnsseed" >> plugin.cfg
 echo "replace github.com/btcsuite/btcd => github.com/gtank/btcd v0.0.0-20191012142736-b43c61a68604" >> go.mod
